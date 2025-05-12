59b35df3aefffc80bc9ca00ad0df051c8980aed0
aszostek <01187292@pw.edu.pl>
2025-05-12 19:15:59 +0200

    completed the backend endpoint and initialised the frontend

---
+++ webapp/backend/main.py
@@ -0,0 +1,42 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from peft import PeftModel
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import torch.nn.functional as F
+import torch
+
+
+app = FastAPI()
+
+base_model = AutoModelForSequenceClassification.from_pretrained(
+    "bert-base-uncased"
+)
+model = PeftModel.from_pretrained(
+    base_model,
+    "./model_directory",
+    local_files_only=True
+)
+tokenizer = AutoTokenizer.from_pretrained(
+    "./model_directory",
+    local_files_only=True
+)
+model.eval()
+
+class InputText(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(input: InputText):
+    inputs = tokenizer(input.text, return_tensors="pt", truncation=True, padding=True)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    logits = outputs.logits
+    probs = F.softmax(logits, dim=1)
+    confidence, predicted_class = torch.max(probs, dim=1)
+
+    return {
+        "label": predicted_class.item(),
+        "confidence": round(confidence.item() * 100, 2)
+    }
